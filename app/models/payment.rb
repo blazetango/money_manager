deb1fded57f2f5befb4397fb4262145b703dd167
blazetango <blazetango@gmail.com>
2015-12-18 17:08:14 +0530

    Added the share per head in the event index page Revert option added to the normal user for payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,9 @@
 class Payment < ActiveRecord::Base
-
+	before_save :update_amount
 	belongs_to :user
 	belongs_to :event
+
+	def update_amount
+    self.amount = (self.event.amount/self.event.users.size)
+	end
 end
